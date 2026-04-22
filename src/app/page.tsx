b50cbadb3124c06b9d58b5318da94dd51420d184
necteau <necteau@gmail.com>
2026-04-22 11:52:50 -0400

    Update page.tsx to force new build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main>
+    <main className="min-h-screen">
       <Hero />
       <About />
       <Amenities />
