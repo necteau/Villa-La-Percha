71175684fd3e94c80e91fbda6c980b861417ef60
necteau <necteau@gmail.com>
2026-04-22 12:03:32 -0400

    Version 2026-04-22-1201

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import Location from "@/components/Location";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
+// Version: 2026-04-22-1201
 export default function Home() {
   return (
     <main className="min-h-screen">
